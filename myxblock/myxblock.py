790732e8d3ab7a8946ed0b2a14c07f29da36da23
Rafael Nissi <rafael.nissi@usp.br>
2020-12-03 13:29:55 -0300

    Limpeza nos códigos

--- myxblock/myxblock.py
+++ myxblock/myxblock.py
@@ -15,14 +15,6 @@ class MyXBlock(XBlock):
 
     fs = Filesystem(help="File system", scope=Scope.user_state_summary)  # pylint: disable=invalid-name
 
-    # Fields are defined on the class.  You can access them in your code as
-    # self.<fieldname>.
-
-    #Sistema recursivo de contas
-    #Cada um deles vai conter uma resposta, e um vetor de argumentos que seriam os valores que o aluno colocará
-    #Pode ter um sistema dentro de outro, assim sendo recursivo
-    #
-
     problemTitle = String(
         default="Title", scope=Scope.settings,
         help="Title of the problem",
@@ -73,11 +65,6 @@ class MyXBlock(XBlock):
         help="Tags of the problem",
     )
 
-    #problemRadioAnswers = String(
-    #    default=["Option A", "Option B", "Option C"], scope=Scope.user_state_summary,
-    #    help="Items of the problem",
-    #)
-
 
     #Resposta desse bloco
     answerSteps = String(
@@ -105,28 +92,6 @@ class MyXBlock(XBlock):
         help="Student's final answer",
     )
 
-    #Operação desse bloco
-    #0 = Soma
-    #1 = Subtração
-    #2 = Multiplicação
-    #3 = Divisão
-    #operation = Integer(
-    #    default=0, scope=Scope.user_state_summary,
-    #    help="Qual operação que será realizada",
-    #)
-
-    ##Um dos valores já presente desse bloco
-    #value1 = Integer(
-    #    default=0, scope=Scope.user_state_summary,
-    #    help="Valor 1 já introduzido",
-    #)
-
-    ##Um dos valores já presente desse bloco
-    #value2 = Integer(
-    #    default=0, scope=Scope.user_state_summary,
-    #    help="Valor 2 já introduzido",
-    #)
-
     def resource_string(self, path):
         """Handy helper for getting resources from our kit."""
         data = pkg_resources.resource_string(__name__, path)
@@ -139,16 +104,6 @@ class MyXBlock(XBlock):
         frag = Fragment(str(html).format(block=self))
         frag.add_css(self.resource_string("static/css/myxblock.css"))
         frag.add_javascript(self.resource_string("static/js/src/myxblock.js"))
-        #if self.fs.exists(u"problem.json"):
-        #    problem = json.load(self.fs.open(u"problem.json"))
-        #    title = problem['title']
-        #    description = problem['description']
-        #    radioAnswers = problem['radioAnswers']
-        #    correcRadioAnswer = problem['correcRadioAnswer']
-        #else:
-        #    with self.fs.open(u'problem.json', 'wb') as file_output:
-        #        file_output.write(json.dumps({'title': 'Teste', 'description': 'Teste', 'radioAnswers': ['Option A', 'Option B', 'Option C'], 'correcRadioAnswer': 'Option A'}).encode())
-        #        file_output.close()
 
         #Também precisa inicializar
         frag.initialize_js('MyXBlock')
@@ -168,16 +123,7 @@ class MyXBlock(XBlock):
 
         if '' in answerArray:
             answerArray =  list(filter(lambda value: value != '', answerArray))
-            #for value in answerArray:
-            #    if value == 'a:
-            #        answerArray.remove('')
 
-        #if data['answer'] == '1':
-        #   return {"answer": self.answerClick}
-        #else:
-        #   return {"answer": 0}
-
-        #if data['answer'] == '1':
         self.answerSteps = answerArray
         self.answerRadio = data['radioAnswer']
         if answerArray[-1] == self.problemCorrectRadioAnswer and self.answerRadio == self.problemCorrectRadioAnswer:
--- myxblock/static/js/src/myxblock.js
+++ myxblock/static/js/src/myxblock.js
@@ -8,20 +8,6 @@ function MyXBlock(runtime, element, data) {
 
         $('#subject', element).text(value.subject);
 
-        //$('#tags', element).text(JSON.parse(value.tags));
-
-        //for (var c in value.tags.{
-        //    var newElement = document.createElement('div');
-        //    newElement.id = value.tags[c]; 
-        //    newElement.className = "car";
-        //    newElement.innerHTML = value.tags[c];
-        //    document.body.appendChild(newElement);
-        //}
-
-
-
-        //$('#tags', element).text(value.tags);
-
         document.getElementById("answer1").value = value.answer1;
         var itsLabel1 = $("[for=" + $("#answer1").attr("id") + "]");
         itsLabel1.text(value.answer1);
@@ -44,16 +30,9 @@ function MyXBlock(runtime, element, data) {
     }
 
     function showAnswer(value) {
-        //var btn = document.createElement("BUTTON");   // Create a <button> element
-        //btn.innerHTML = "CLICK ME";                   // Insert text
-        //document.body.appendChild(btn);               // Append <button> to <body>
-
-        //$('.answer', element).text(value.answer);
         $('#hint', element).text(value.hint);
     }
 
-    //showAnswer(data)
-    
     var send_answer = runtime.handlerUrl(element, 'send_answer');
     var getInitialData = runtime.handlerUrl(element, 'initial_data');
 
